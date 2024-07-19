14fe475a0d042f6683c9c9f276b10bda7f87a9a8
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-07-19 10:17:15 -0300

    Adicionei mais alguns produtos

--- src/assets/Infos/infos.js
+++ src/assets/Infos/infos.js
@@ -1,29 +1,119 @@
 export const productsInfo = {
-    "cards": [
-      {
-        "imagem": "/imagens/photo1711140339.jpeg",
-        "titulo": "Rosa Amazonica",
-        "descricao": "Hidratação profunda e rejuvenescimento com o poder do óleo de Rosa Mosqueta da Amazônia.",
-        "link": "https://ev.braip.com/ref?pv=proeo8oe&af=afizj5v0on"
-      },
-      {
-        "imagem": "/imagens/HarmonyHair.PNG",
-        "titulo": "Harmony Hair",
-        "descricao": "Cápsulas para auxiliar no crescimento, fortalecimento e combate à queda dos cabelos.",
-        "link": "https://ev.braip.com/ref?pv=prol9enx&af=afi3vzxexd"
-      },
-      {
-        "imagem": "/imagens/HidraPele.JPG",
-        "titulo": "Hidra Pele",
-        "descricao": "Sérum facial hidratante que reduz rugas, manchas e controla a oleosidade.",
-        "link": "https://ev.braip.com/ref?pv=prow2zwy&af=afimedo6o1"
-      },
-      {
-        "imagem": "/imagens/KitSOSCrescimento.PNG",
-        "titulo": "Kit SOS Crescimento (Crescimento Capilar)",
-        "descricao": "Tratamento capilar que promete acelerar o crescimento dos fios, reduzir a queda e fortalecer os cabelos.",
-        "link": "https://ev.braip.com/ref?pv=prov6q42&af=afi11yoeo8"
-      }
-    ]
-  };
-  
+  "cards": [
+    {
+      "imagem": "/imagens/photo1711140339.jpeg",
+      "titulo": "Rosa Amazonica",
+      "descricao": "Hidratação profunda e rejuvenescimento com o poder do óleo de Rosa Mosqueta da Amazônia.",
+      "link": "https://ev.braip.com/ref?pv=proeo8oe&af=afizj5v0on"
+    },
+    {
+      "imagem": "/imagens/HarmonyHair.PNG",
+      "titulo": "Harmony Hair",
+      "descricao": "Cápsulas para auxiliar no crescimento, fortalecimento e combate à queda dos cabelos.",
+      "link": "https://ev.braip.com/ref?pv=prol9enx&af=afi3vzxexd"
+    },
+    {
+      "imagem": "/imagens/HidraPele.JPG",
+      "titulo": "Hidra Pele",
+      "descricao": "Sérum facial hidratante que reduz rugas, manchas e controla a oleosidade.",
+      "link": "https://ev.braip.com/ref?pv=prow2zwy&af=afimedo6o1"
+    },
+    {
+      "imagem": "/imagens/KitSOSCrescimento.PNG",
+      "titulo": "Kit SOS Crescimento (Crescimento Capilar)",
+      "descricao": "Tratamento capilar que promete acelerar o crescimento dos fios, reduzir a queda e fortalecer os cabelos.",
+      "link": "https://ev.braip.com/ref?pv=prov6q42&af=afi11yoeo8"
+    },
+    {
+      "imagem": "/imagens/lola.jpg",
+      "titulo": "Lola Cosmetics Morte Súbita Leave-in 250ml",
+      "descricao": "Leave-in para hidratação e nutrição dos cabelos.",
+      "link": "https://s.shopee.com.br/7fFXlQrsHr?share_channel_code=1"
+    },
+    {
+      "imagem": "/imagens/NutriEnrich.jpg",
+      "titulo": "Kit Shampoo e Condicionador 1L Nutri Enrich",
+      "descricao": "Kit de shampoo e condicionador para nutrição intensa.",
+      "link": "https://s.shopee.com.br/8pRV9YNRGx?share_channel_code=1"
+    },
+    {
+      "imagem": "/imagens/ELSEVEGLYCOLIC.jpg",
+      "titulo": "Kit Elseve Glycolic Gloss Shampoo + Cond + Máscara L'oréal Paris",
+      "descricao": "Kit de cuidados capilares para brilho e maciez.",
+      "link": "https://s.shopee.com.br/3q2pCLTGXQ?share_channel_code=1"
+    },
+    {
+      "imagem": "/imagens/BodySplash.jpg",
+      "titulo": "Body Splash Corporal 250ml Victoria's Secret",
+      "descricao": "Body splash com fragrância duradoura.",
+      "link": "https://s.shopee.com.br/40MFOYqurM?share_channel_code=1"
+    },
+    {
+      "imagem": "/imagens/HidratanteNIVEA.jpg",
+      "titulo": "Kit Loção Hidratante NIVEA Lotion Express Pele",
+      "descricao": "Kit de loção hidratante para pele seca.",
+      "link": "https://s.shopee.com.br/9zdSXYIEWQ?share_channel_code=1"
+    },
+    {
+      "imagem": "/imagens/NIVEASabonete.jpg",
+      "titulo": "NIVEA Sabonete Líquido Óleo de Banho 200ml",
+      "descricao": "Sabonete líquido com óleo de banho para hidratação.",
+      "link": "https://s.shopee.com.br/8UoekmAjiT?share_channel_code=1"
+    },
+    {
+      "imagem": "/imagens/BarraNIVEA.jpg",
+      "titulo": "Sabonete em Barra NIVEA Creme Care 90g",
+      "descricao": "Sabonete em barra para limpeza e hidratação da pele.",
+      "link": "https://s.shopee.com.br/6KkAAm6fck?share_channel_code=1"
+    },
+    {
+      "imagem": "/imagens/LoçãoHidratanteMilk.jpg",
+      "titulo": "NIVEA Kit Loção Hidratante Milk pele seca a extrasseca 400m",
+      "descricao": "Kit de loção hidratante para pele seca a extrasseca.",
+      "link": "https://s.shopee.com.br/7AJHAHqhdh?share_channel_code=1"
+    },
+    {
+      "imagem": "/imagens/Cuide-SeBem.jpg",
+      "titulo": "Cuide-Se Bem Deleite, hidratante, body splash, creme de maos, OBoticario",
+      "descricao": "Kit de cuidados corporais com hidratante, body splash e creme de mãos.",
+      "link": "https://s.shopee.com.br/qPDcXDF0v?share_channel_code=1"
+    },
+    {
+      "imagem": "/imagens/PÓFACIAL.jpg",
+      "titulo": "PÓ FACIAL VEGANO ROSA MOSQUETA SUPER PODERES",
+      "descricao": "Pó facial vegano com óleo de Rosa Mosqueta.",
+      "link": "https://s.shopee.com.br/8pRV9C1WmN?share_channel_code=1"
+    },
+    {
+      "imagem": "/imagens/Gecomomo.jpg",
+      "titulo": "Gecomomo Creme Blush Líquido",
+      "descricao": "Creme blush líquido para um toque de cor natural.",
+      "link": "https://s.shopee.com.br/6AQjyGC4hh?share_channel_code=1"
+    },
+
+    {
+      "imagem": "/imagens/Blend.jpg",
+      "titulo": "Blend de Óleos - Crescimento Capilar - Côco e Alecrim 100% Natural",
+      "descricao": "Óleos 100% naturais para crescimento capilar, com coco e alecrim.",
+      "link": "https://s.shopee.com.br/20bB0XKklv?share_channel_code=1"
+    },
+    {
+      "imagem": "/imagens/LolaCosmetics.jpg",
+      "titulo": "Lola Cosmetics Argan Oil Óleo Capilar 50ml",
+      "descricao": "Óleo capilar de Argan para hidratação e brilho.",
+      "link": "https://s.shopee.com.br/LSx1SVlSU?share_channel_code=1"
+    },
+    {
+      "imagem": "/imagens/KitSiage.jpg",
+      "titulo": "Kit Siàge Pro Cronology Cronograma Capilar Acelerado Completo Eudora",
+      "descricao": "Kit completo para cronograma capilar acelerado.",
+      "link": "https://s.shopee.com.br/10idofKy6p?share_channel_code=1"
+    },
+    {
+      "imagem": "/imagens/BocaRosaMáscara.jpg",
+      "titulo": "Boca Rosa Máscara de Cílios Meu Volumão By Payot",
+      "descricao": "Máscara de cílios para volume intenso.",
+      "link": "https://s.shopee.com.br/5fUTNC9E8H?share_channel_code=1"
+    }
+  ]
+};
